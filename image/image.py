4e7d183495552812a5c2fb54488af112da312247
Adefemi <adefemiadeyanju101@hotmail.com>
2025-01-22 13:07:47 +0100

    minor changes

--- image/image.py
+++ image/image.py
@@ -6,12 +6,20 @@ import numpy as np
 dcm = pydicom.dcmread('image/1459541791.dcm')
 image = dcm.pixel_array
 
+dcm1 = pydicom.dcmread('image/1864590858.dcm')
+image1 = dcm.pixel_array
+
 # Normalize
 image = (image - np.min(image))/(np.max(image) - np.min(image)) * 255
 image = image.astype(np.uint8)
 
+dcm1 = pydicom.dcmread('image/1864590858.dcm')
+image1 = dcm.pixel_array
+
+
 # Resize
 image = cv.resize(image, (500,500), interpolation=cv.INTER_CUBIC)
+
 
 cv.imshow('DICOM Image',image)
 cv.waitKey(0)
